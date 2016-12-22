b965be9cb21681417613fcb77e6510dba8acb692
sewes <dewenhs@yandex.ru>
2016-12-23 02:19:56 +0400

    5 lesson by Sorax

---
+++ 5 lesson by Sorax/main.js
@@ -0,0 +1,25 @@
+var N = new Number(50),
+	n = 25;
+console.log(typeof N);
+console.log(typeof n);
+
+console.log(n.toFixed(3));
+console.log(N.toFixed(4));
+console.log(5 .toFixed(5));
+
+console.log("");
+
+var i = 10,
+	n = 10;
+console.log(++i);
+console.log(i);
+(n++);
+
+console.log("");
+
+console.log(5 > 10);
+console.log(4 < 3);
+console.log(10 >= 4);
+console.log(13 <= 76);
+console.log(10 === 10);
+console.log(10 !== 10); 
