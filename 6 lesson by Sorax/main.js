0c64ac71d5b2903628894594345070f6f16d183e
sewes <dewenhs@yandex.ru>
2016-12-23 13:03:00 +0400

    6 lesson by Sorax

---
+++ 6 lesson by Sorax/main.js
@@ -0,0 +1,21 @@
+console.log("Some string");
+
+console.log("some 'another' string");
+
+console.log("Hello world".length);
+
+console.log("Another \n\tvery \n\t\tlong string")
+
+console.log(" ");
+
+var string = "Sometimes the same is different";
+console.log(string.length);
+console.log(string.charAt(5));
+console.log(string.substring(10));
+console.log(string.substring(10,21));
+console.log(string.slice(10));
+console.log(string.substr(10,5));
+console.log(string.indexOf("me"));
+console.log(string.split(" "));
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
