7b7a21c212cdffad25a5aa58f8861edee26ae5a0
sewes <dewenhs@yandex.ru>
2016-12-24 18:31:56 +0400

    9 lesson by Sorax

---
+++ 9 lesson by Sorax/main.js
@@ -0,0 +1,30 @@
+console.log("555");
+console.log(typeof(Number("555")));
+console.log(String(4433));
+console.log(typeof(String(4433)));
+console.log(Boolean(1));
+console.log(typeof(Boolean(1)));
+
+console.log(!!5);
+console.log(!!0);
+console.log(typeof(345 + ""));
+console.log(typeof + "454");
+
+console.log(" ");
+var number = 22;
+console.log(typeof number.toString());
+number = 45;
+console.log(number.toString(3));
+number = 5;
+console.log(number.toString(2));
+
+console.log(typeof false.toString());
+
+console.log(" ");
+
+console.log(!!"hi");
+console.log(parseInt("4 px"));
+console.log(+true);
+console.log(+false);
+
+
