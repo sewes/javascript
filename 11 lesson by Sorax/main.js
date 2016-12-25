d206545c3f7140ba78400b43ac423782627e5c1c
sewes <dewenhs@yandex.ru>
2016-12-25 23:24:08 +0400

    11 lesson by Sorax

---
+++ 11 lesson by Sorax/main.js
@@ -0,0 +1,12 @@
+var x = 15,
+	text;
+
+text = x > 10 ? "x > 10" : "x < 10 or x = 10";
+console.log(text);
+
+var p = 5,
+	text1;
+
+text1 = p > 5 ? "p > 5" : p = 5 ? "p = 5" : "p < 5";
+
+console.log(text1); 
