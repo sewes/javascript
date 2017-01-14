a6168928052ba36527469b8cd9eebad0f122bd28
sewes <dewenhs@yandex.ru>
2017-01-14 23:39:39 +0400

    24 lesson by Sorax

--- 24 lesson by Sorax/main.js
+++ 24 lesson by Sorax/main.js
@@ -14,3 +14,28 @@ console.log(months.length);
 
 console.log("");
 
+var array = ["some string", "another string", "third string"];
+
+console.log(array);
+console.log(array.join(" || "));
+console.log(array.reverse());
+console.log(array);
+
+console.log(array.sort());
+console.log(array.concat("hello", ["even more", "string"]));
+console.log(array.splice(1, 1, "dewen", "webdev"));
+console.log(array);
+
+var array1 = ["ss", "pp", "ee", "ff"];
+array1.push("js");
+console.log(array1);
+array1.unshift("se");
+console.log(array1);
+
+console.log(array1.pop());
+console.log(array1.pop());
+console.log(array1.pop());
+console.log(array1);
+
+console.log(array1.shift());
+console.log(array1);
