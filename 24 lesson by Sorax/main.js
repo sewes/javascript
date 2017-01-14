7d0ee38fb66a794709fa772c59f96a2f986d13f0
sewes <dewenhs@yandex.ru>
2017-01-14 23:22:03 +0400

    24 lesson by Sorax

---
+++ 24 lesson by Sorax/main.js
@@ -0,0 +1,16 @@
+var months = ["j", "f", "m", "a"];
+
+months[4] = "may";
+
+console.log(months);
+console.log(months.length);
+
+months[months.length] = "june";
+console.log(months);
+
+months[15] = "something";
+console.log(months);
+console.log(months.length);
+
+console.log("");
+
